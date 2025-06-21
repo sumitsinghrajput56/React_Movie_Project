1462c15b4914b0f0d5e5f24f405e2f6d17c1d122
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-06-21 17:43:33 +0530

    api integreted and loading

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,19 @@
+import '../css/MovieCard.css';
+
+
 function MovieCard({ movie }) {
   function onFavoriteClick() {}
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.title} />
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
         <div className="movie-overlay">
           <button className="favorite-btn" onClick={onFavoriteClick}>❤️</button>
         </div>
       </div>
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <p>{movie.release_date}</p>
+        <p>{movie.release_date?.split('-')[0]}</p>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
+import "../css/Home.css";
+import { searchMovies, getPopularMovies } from "../services/api";
 
 function Home() {
-    const [searchQuery,setSearchQuery]=useState("");
-  const movies = [
-    { id: 1, title: "Ashiqui 2", release_date: 2020 },
-    { id: 2, title: "BlockBluster", release_date: 2021 },
-    { id: 3, title: "Karan Arjun", release_date: 2022 },
-  ];
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const handleSearch=(e)=>{
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        setError("failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
+
+  const handleSearch = (e) => {
     e.preventDefault();
     setSearchQuery("");
-
-  }
+  };
   return (
     <>
       <div className="home">
         <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="search for movies..." className="search-input" value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)}/>
-            <button type="submit" className="search-button">search</button>
+          <input
+            type="text"
+            placeholder="search for movies..."
+            className="search-input"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button type="submit" className="search-button">
+            search
+          </button>
         </form>
-        <div className="movies-grid">
-          {movies.map((movie) =>( 
-            movie.title.toLowerCase().startsWith(searchQuery) &&
-            <MovieCard movie={movie} key={movie.id} />
-            ))}
-        </div>
+        {loading ? (
+          <div className="loading">Loading...</div>
+        ) : (
+          <div className="movies-grid">
+            {movies.map(
+              (movie) =>
+                movie.title.toLowerCase().startsWith(searchQuery) && (
+                  <MovieCard movie={movie} key={movie.id} />
+                )
+            )}
+          </div>
+        )}
       </div>
     </>
   );
 }
 
-export default Home
+export default Home;
